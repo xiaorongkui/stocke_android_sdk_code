a214402fe6d6d85026a731e3b1f5b1cb6f1612a2
xiaorongkui <dovexiaoen@163.com>
2018-09-10 09:33:09 +0800

    Initial commit

---
+++ app/src/main/java/com/stocke/sdk/MainActivity.java
@@ -0,0 +1,13 @@
+package com.stocke.sdk;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(sdk.stocke.com.stocke_android_sdk_code.R.layout.activity_main);
+    }
+}
